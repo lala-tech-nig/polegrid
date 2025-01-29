a1996c8fecc43dcb454b8b98a3a401dc1c58b154
Akandedaniel7 <akandedaniel86@gmail.com>
2025-01-29 12:00:08 +0100

    sections assfef

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import SectionFooter from "./SectionFooter";
 import './footer.css';
 
@@ -9,12 +10,13 @@ const Footer = () => {
     <div className="container">
       <h3 className="sitename">Pole<span className="footer_card1">Grid</span></h3>
       <p>PoleGrid is your trusted partner in connecting opportunities.</p>
+      <Link className="termsandcondtionslink" to='/termsandconditions'>Terms and Conditions</Link>
       <div className="social-links d-flex justify-content-center">
-        <a href=""><i className="bi bi-twitter-x"></i></a>
+        {/* <a href=""><i className="bi bi-twitter-x"></i></a> */}
         <a href=""><i className="bi bi-facebook"></i></a>
         <a href=""><i className="bi bi-instagram"></i></a>
         {/* <a href=""><i className="bi bi-skype"></i></a> */}
-        <a href=""><i className="bi bi-linkedin"></i></a>
+        {/* <a href=""><i className="bi bi-linkedin"></i></a> */}
       </div>
       <div className="container">
         <div className="copyright">
