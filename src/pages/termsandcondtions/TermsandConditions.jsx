344f3a9659d526bbffa9f508c6621f7e480e628b
Daniel Akande <akandedaniel86@gmail.com>
2025-05-31 17:54:18 +0100

    comited terms

--- src/pages/termsandcondtions/TermsandConditions.jsx
+++ src/pages/termsandcondtions/TermsandConditions.jsx
@@ -11,86 +11,72 @@ const TermsandConditions = () => {
       <div className='container'>
        
         
-      <h4 className='terms_heading2'><b>PoleGrid Terms of Service</b></h4>
+      <h4 className='terms_heading2'><b>PoleGrid Solutions Terms of Service</b></h4>
 
-   <p>Welcome to Polegrid, operated by Polegrid Limited, registered in England with company number 14549218.
-
-By using our Platform, you agree to comply with and be bound by these Terms of Service. Please read them carefully, as they outline the relationship between you (the “user”) and Vacua Limited (“we” or “us”) and the terms governing your use of our services.</p>
+   <p>Welcome to Polegrid Solutions, Registered in Nigeria and verified by the Special Control Units Against Money Laundering. By using our Platform, you agree to comply with these Terms of Service. Please read them carefully.
+</p>
 <br />
 
 <ol>
 
 
 <li> <b>Introduction</b> <br />
-These Terms and Conditions ("Terms") govern the use of Polegrid Solutions' services ("Services") and website ("Website").
-By registering for our Services, you ("Client") agree to be bound by these Terms.
+These Terms and Conditions ("Terms") govern the use of Polegrid Solutions' services ("Services") and website ("Website"). By registering for our Services, you ("Client") agree to be bound by these Terms.
 </li>
 
 <br />
 
 <li> <b>Definitions</b> 
 <br/>
-Property" refers to the land or site registered by the Client for our Services.
- "Service" refers to the telecoms infrastructure installation and related services provided by Polegrid Solutions.
-"Service Provider" refers to the third-party telecoms service provider or money machine ( atm) provider selected by the Client.
+- "Property" refers to the land or site registered by the Client for our Services. <br />
+- "Service" refers to telecom infrastructure installation, ATM machine installation, and other related services facilitated by Polegrid Solutions.<br />
+- "Service Provider" refers to third-party telecom companies, tower companies, financial institutions, and other related organizations.
 </li> 
 <br />
 
 <li> <b>Registration and Payment</b>
 <br />
-The Client must provide accurate and complete registration information.
- The Client must pay the one-time registration fee of N15,500.
- Payment is non-refundable, except as provided in these Terms.
+- The Client (Landlord or Landowner or supermarkets or shop owners) must provide accurate and complete registration information. <br />
+- The Client must pay a one-time consultation and registration fee of ₦15,500. Payment is non-refundable, except as provided in these Terms.
 </li>
 <br />
 
 <li> <b> Service Provision</b>
 <br />
 
-Polegrid Solutions will facilitate the installation of telecoms or money machine infrastructure on the Client's Property.
-The Client must grant access to the Property for installation and maintenance purposes.
-Polegrid Solutions reserves the right to modify or terminate the Services at any time.
+- Polegrid Solutions will facilitate the installation of the above services on the Client's Property. <br />
+- The Client must grant access to the Property for installation and maintenance purposes. <br />
+- Polegrid Solutions reserves the right to modify or terminate the Services at any time.
 </li>
 
 <li> <b>Client Obligations</b>
 <br />
- The Client must comply with all applicable laws and regulations.
-The Client must ensure the Property is suitable for infrastructure installation.
- The Client must notify Polegrid Solutions of any changes to the Property or Service requirements.
+- The Client must comply with all applicable laws and regulations. <br />
+- The Client must ensure the Property is suitable for infrastructure installation. <br />
+- The Client must notify Polegrid Solutions of any changes to the Property or Service requirements.
 </li> <br />
 
 <li> <b> Liability and Indemnification</b>
 <br />
-Polegrid Solutions shall not be liable for any damages or losses arising from the Services, except to the extent caused by our gross negligence or willful misconduct.
-The Client shall indemnify and hold harmless Polegrid Solutions against any claims, damages, or expenses arising from the Client's breach of these Terms.
+- Polegrid Solutions shall not be liable for any damages or losses arising from the Services, except to the extent caused by our gross negligence or willful misconduct. <br />
+- The Client shall indemnify and hold harmless Polegrid Solutions against any claims, damages, or expenses arising from the Client's breach of these Terms.
 </li><br />
 
 <li> <b> Confidentiality</b>
 <br />
-Polegrid Solutions and the Client shall keep confidential all information and data exchanged in connection with the Services.
-This confidentiality obligation shall survive termination of these Terms.
+- Polegrid Solutions and the Client shall keep confidential all information and data exchanged in connection with the Services. <br />
+- This confidentiality obligation shall survive termination of these Terms.
 </li > <br />
 
 <li> <b> Termination</b>
 <br />
-Polegrid Solutions may terminate these Terms and the Services at any time, with or without cause.
-Upon termination, the Client shall immediately cease using the Services.
+- Polegrid Solutions may terminate these Terms and the Services at any time, with or without cause. <br />
+- Upon termination, the Client shall immediately cease using the Services.
 </li> <br />
 
 <li> <b>Governing Law</b>
-These Terms shall be governed by and construed in accordance with the laws of Federal Republic of Nigeria.
-Any disputes arising from these Terms shall be resolved through [Dispute Resolution Process].
+These Terms shall be governed by and construed in accordance with the laws of the Federal Republic of Nigeria.
 </li> <br />
-
-<li> <b>Changes to Terms</b>
-Polegrid Solutions reserves the right to modify these Terms at any time.
-Changes shall be effective upon posting on the Website.
-</li> <br />
-
-<li> <b>Acceptance</b>
-By registering for our Services, the Client acknowledges that they have read, understood, and agreed to be bound by these Terms.
-</li> <br />
-
 </ol>
 
 </div>
